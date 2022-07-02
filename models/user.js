b0611d5a270c96c5283178bf7bc7867b7586984d
Luydmyla <lytic.ok@gmail.com>
2022-07-03 02:56:20 +0300

    some fixes

--- models/user.js
+++ models/user.js
@@ -26,6 +26,15 @@ const userSchema = Schema(
   { versionKey: false, timestamps: true }
 );
 
+// 2 спосіб для зберігання паролей захешированих в базі
+userSchema.methods.setPassword = function (password) {
+  this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+// добавляємо ще один метод для порівняння паролів що ввів користувач з тим, що є в базі захеширований
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
 // створюємо джоі-схему
 const joiRegisterSchema = Joi.object({
   password: Joi.string().min(8).required(),
